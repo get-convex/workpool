51208057188d6eaae186c529c71127ad7c42ce6d
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-05-12 20:06:02 -0700

    statically host the dashboard

--- example/convex/convex.config.ts
+++ example/convex/convex.config.ts
@@ -1,6 +1,7 @@
 import { defineApp } from "convex/server";
 import workpool from "@convex-dev/workpool/convex.config";
 import workpoolOld from "@convex-dev/workpool-old/convex.config";
+import staticHosting from "@convex-dev/static-hosting/convex.config";
 
 const app = defineApp();
 app.use(workpool, { name: "smallPool" });
@@ -8,5 +9,6 @@ app.use(workpool, { name: "bigPool" });
 app.use(workpool, { name: "serializedPool" });
 app.use(workpool, { name: "testWorkpool" });
 app.use(workpoolOld, { name: "oldWorkpool" });
+app.use(staticHosting);
 
 export default app;
