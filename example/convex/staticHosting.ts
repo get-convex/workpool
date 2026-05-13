3341ee6449b325e043c9e0c0a62a2f3cffc9bf18
Ian Macartney <366683+ianmacartney@users.noreply.github.com>
2026-05-12 20:06:02 -0700

    statically host the dashboard

---
+++ example/convex/staticHosting.ts
@@ -0,0 +1,18 @@
+import {
+  exposeDeploymentQuery,
+  exposeUploadApi,
+} from "@convex-dev/static-hosting";
+import { components } from "./_generated/api";
+
+export const {
+  generateUploadUrl,
+  generateUploadUrls,
+  recordAsset,
+  recordAssets,
+  gcOldAssets,
+  listAssets,
+} = exposeUploadApi(components.selfHosting);
+
+export const { getCurrentDeployment } = exposeDeploymentQuery(
+  components.selfHosting,
+);
